3d0a19387171a81dc5ba790af63febc123da7b96
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-09-11 09:41:19 -0400

    1.7.1 Inheritance

---
+++ src/main/kotlin/introduction/inheritance.kt
@@ -0,0 +1,21 @@
+package introduction
+
+
+open class Dog {
+    open fun sayHello() {
+
+        println("wow wow!")
+    }
+}
+
+class Yorkshire : Dog() {
+    override fun sayHello() {
+        println("wif wif!")
+    }
+}
+
+fun main() {
+    val dog: Dog = Yorkshire();
+
+    dog.sayHello();
+}
