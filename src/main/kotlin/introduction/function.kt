3a22e7d32ff41642479c0a06502304d68444b668
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-09-11 08:17:11 -0400

    1.2.1 Functions

---
+++ src/main/kotlin/introduction/function.kt
@@ -0,0 +1,26 @@
+package introduction
+
+/*Unit class corresponds to the void type in Java  */
+fun printMessage(message: String): Unit {
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    printMessage("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
+fun main() {
+    printMessage("Hello")
+    printMessageWithPrefix("Hello", "Log")
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+    println(sum(1, 2))
+    println(multiply(2, 4))
+
+}
