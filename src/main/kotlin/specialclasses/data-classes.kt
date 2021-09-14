65eba6338061344d85fbb2bb9c1bc848bfa913d9
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-09-13 20:31:42 -0400

    3.1.1 Data Classes

---
+++ src/main/kotlin/specialclasses/data-classes.kt
@@ -0,0 +1,38 @@
+package specialclasses
+
+
+data class User(val name: String, val id: Int) {
+
+    override fun equals(other: Any?) = other is User && other.id == this.id
+    override fun hashCode(): Int = this.id;
+}
+
+
+fun main() {
+    val user = User("Ronei", 1)
+
+    println(user)
+
+    val secondUser = User("Ronei", 1)
+    val thirdUser = User("Max", 2)
+
+    println("user == secondUser: ${user == secondUser}")
+    println("user == thirdUser: ${user == thirdUser}")
+
+
+    println(user.hashCode())
+    println(secondUser.hashCode())
+    println(thirdUser.hashCode())
+
+
+    println(user.copy())
+    println(user === user.copy())
+
+    println(user.copy(name = "Max"))
+    println(user.copy(id = 3))
+
+    println("name = ${user.component1()}")
+    println("id = ${user.component2()}")
+
+
+}
