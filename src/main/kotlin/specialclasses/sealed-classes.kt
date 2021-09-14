a955c76ac3ade31e32acec31fda5c0d634282e10
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-09-13 20:40:44 -0400

    3.1.3 Sealed Classes

---
+++ src/main/kotlin/specialclasses/sealed-classes.kt
@@ -0,0 +1,18 @@
+package specialclasses
+
+sealed class Mammal(val name: String)
+
+class Cat(val catName: String) : Mammal(catName)
+class Human(val humanName: String, val job: String) : Mammal(humanName)
+
+
+fun greetMammal(mammal: Mammal): String {
+    return when (mammal) {
+        is Human -> "Hello ${mammal.name} You're working as a ${mammal.job}";
+        is Cat -> "Hello ${mammal.name}"
+    }
+}
+
+fun main() {
+    println(greetMammal(Cat("Snowy")))
+}
