4040ca48656cfb15d593a7565191548574f33943
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-09-16 15:44:25 -0400

    6.1.1 Let

---
+++ src/main/kotlin/scope-functions/let.kt
@@ -0,0 +1,40 @@
+package `scope-functions`
+
+
+fun main() {
+    val empty = "test".let {
+        customPrint(it)
+        it.isEmpty()
+    }
+
+    println(" is empty : $empty")
+
+    fun printNonNull(str: String?) {
+        println("Printing \"$str\":")
+        str?.let {
+            print("\t")
+            customPrint(it)
+            println()
+        }
+    }
+
+    fun printIfBothNonNull(strOne: String?, strTwo: String?) {
+        strOne?.let { firstString ->
+            strTwo?.let { secondString ->
+                customPrint("$firstString : $secondString")
+                println()
+            }
+        }
+    }
+
+    printNonNull(null)
+    printNonNull("my string")
+    printIfBothNonNull("First", "Second")
+}
+
+fun customPrint(it: String) {
+    println("Custom Print $it");
+}
+
+
+
